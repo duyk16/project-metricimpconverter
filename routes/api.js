fd731bf3461e442248d83e11c40266f586e71677
Monkey <duyk16@gmail.com>
2019-10-03 19:51:28 +0700

    update controller nam

--- routes/api.js
+++ routes/api.js
@@ -9,7 +9,7 @@
 'use strict';
 
 const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convert.controller.js/index.js');
+const ConvertHandler = require('../controllers/convertHandler');
 
 module.exports = function (app) {
   
